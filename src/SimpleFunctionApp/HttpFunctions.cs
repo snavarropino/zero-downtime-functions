8ed22f161426e0b764813259964819d8679b4ea8
Sergio Navarro Pino <sergio.navarropino@gmail.com>
2021-06-12 11:07:03 +0200

    Adding http function

---
+++ src/SimpleFunctionApp/HttpFunctions.cs
@@ -0,0 +1,25 @@
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace SimpleFunctionApp
+{
+    public static class HttpFunctions
+    {
+        private const int Version = 1;
+
+        [Function("Version")]
+        public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            await Task.Delay(300);
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            await response.WriteStringAsync($"This is version {Version}");
+
+            return response;
+        }
+    }
+}
